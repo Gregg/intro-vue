a56a09d8d8f7d1ec693b02ba48f232dc735ac81c
Adam Jahr <adamjahr@gmail.com>
2017-11-21 15:54:48 -0500

    tabs working

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+////////////////////////////////////////////////////PRODUCT-DISPLAY//
+
 Vue.component('product-display', {
   props: ['title', 'desc','price', 'stocked', 'image'],
   template: `
@@ -18,24 +20,80 @@ Vue.component('product-display', {
           <p>Price: {{ price }}</p>
           <p v-if="stocked">In Stock</p>
           <p v-else="stocked">Out of Stock</p>
+          <div style="border: 1px solid black; width: 30%;">
+            <p>Color Options:</p>
+            <div style="background-color: red; width: 40px; height: 30px; margin: 5px;"></div>
+            <div style="background-color: blue; width: 40px; height: 30px; margin: 5px;"></div>
+          
+          </div>
         </div>
       </div>
     </div>
   `
 })
 
+////////////////////////////////////////////////////////PRODUCT-TABS//
+
 Vue.component('tabs', {
   template: `
-    <div class="tabs container">
+  <div class="container">
+    <div class="tabs">
       <ul>
-        <li class="is-active"><a>Reviews</a></li>
-        <li><a>Details</a></li>
+        <li v-for="tab in tabsArray" :class="{ 'is-active' : tab.isActive }">
+          <a @click="selectTab(tab)">{{ tab.name }}</a>
+        </li>
       </ul>
+    </div>
+
+    <div>
+      <slot></slot>
+    </div>
   </div>
-  `
+  `,
+  data() {
+    return {
+      tabsArray: []
+    }
+  },
+  mounted() {
+    this.tabsArray = this.$children;
+  },
+  methods: {
+    selectTab(clickedTab) {
+      this.tabsArray.forEach(tab =>
+        tab.isActive = (tab === clickedTab)
+      )
+    }
+  }
+})
+
+////////////////////////////////////////////////////////PRODUCT-TAB//
+
+Vue.component('tab', {
+  template: `
+    <div v-show="isActive">
+      <slot></slot>
+    </div>
+  `,
+  props: { 
+    name: { required: true },
+    selected: { default: false }
+    // brand: String,
+    // material: String,
+    // shipping: Number
+  },
+  data() {
+    return {
+      isActive: false
+    }
+  },
+  mounted() {
+    this.isActive = this.selected
+  }
 })
 
 
+///////////////////////////////////////////////////////////INSTANCE//
 
 var app = new Vue({
   el: '#app',
@@ -45,10 +103,22 @@ var app = new Vue({
     product: {
       id: 1,
       title: 'Flannel Shirt',
-      desc: 'The sexiest shirt you will ever wear',
+      desc: 'The finest shirt you will ever wear',
       price: 25.00,
       image: "https://s3.amazonaws.com/images.gearjunkie.com/uploads/2015/09/stormy-kromer-flannel.png",
       stocked: true,
+      variants: [
+        {
+          id: 234,
+          color: "Red",
+          quantity: 5,
+          image: ""
+        },
+        {
+          id: 235,
+          color: "Blue"
+        }
+      ],
       details: {
         brand: 'Vuetiful',
         material: 'organic cotton',
@@ -57,5 +127,3 @@ var app = new Vue({
     }
   }
 })
-
-
